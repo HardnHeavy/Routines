77cf1f2dfb44df434399ab0b90bfca69d0bbe702
HardnHeavy <harry.trautmann@codemachinery.net>
2016-03-14 17:20:51 +0100

    Added support for continuous integration,e. g. via Jenkins.

---
+++ Assets/Routines/Scripts/Helpers/CIBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif // UNITY_EDITOR
+
+public class CIBuilder : MonoBehaviour {
+#if UNITY_EDITOR
+    static string[] SCENES = FindEnabledEditorScenes();
+
+    static string APP_NAME = "Routines";
+    static string TARGET_DIR = "target";
+
+    [MenuItem("Custom/CI/Make Windows Build")]
+    static void PerformWindowsBuild() {
+        string target_dir = APP_NAME + ".exe";
+        GenericBuild(SCENES, TARGET_DIR + "\\" + target_dir, BuildTarget.StandaloneWindows, BuildOptions.None);
+    }
+
+    private static string[] FindEnabledEditorScenes() {
+        List<string> EditorScenes = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes) {
+            if (!scene.enabled) continue;
+            EditorScenes.Add(scene.path);
+        }
+        return EditorScenes.ToArray();
+    }
+
+    static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options) {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
+        string res = BuildPipeline.BuildPlayer(scenes, target_dir, build_target, build_options);
+        if (res.Length > 0) {
+            throw new Exception("BuildPlayer failure: " + res);
+        }
+    }
+#endif // UNITY_EDITOR
+}// class
